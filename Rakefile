5bb44d87d4b2680ecbf94ac80cab7d7e06579d66
Quinn Rohlf <qr@qrohlf.com>
2014-02-16 17:36:21 -0800

    I am forcing myself to stop working on this for a day

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
 require './app'
 require 'sinatra/activerecord/rake'
+
+namespace :db do 
+    task :seed do
+        seed_file = "./db/seeds.rb"
+        puts "Seeding database from: #{seed_file}"
+        load(seed_file) if File.exist?(seed_file)
+    end
+end
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,44 @@
-$( "#main" ).on( "click", ".button.next", function() {
-  var current = $(this).parents(".slide").first();
-  var next = current.next();
-  console.log(current);
-  current.toggleClass("active");
-  next.toggleClass("active");
+$(document).ready(function() {
+    var currbg = $(".background").last();
+    var nextbg = $(".background").first();
+    var tmp;
+
+    $( "#main" ).on( "click", ".button.next", function() {
+      var current = $(this).parents(".slide").first();
+      var next = current.next();
+      
+      if(next.attr('data-prefetch-task')) getTask();
+
+      // Crossfade the divs
+      current.fadeOut(400, function() {
+        next.fadeIn();
+      });
+
+      // Crossfade the backgrounds
+      currbg.fadeTo(800, 0, function() {
+        currbg.remove();
+        // swap
+        currbg = nextbg;
+        currbg.css("z-index", -2);
+
+        // preload the next background
+        var preload = $(next.next()[0]).attr("data-bg");
+        if (preload == undefined) return;
+        console.log(preload);
+        nextbg = $('<div class="background" style="background-image: url('+preload+'); z-index: -3;"> </div>');
+        $('body').append(nextbg);
+
+      });
+      nextbg.fadeIn(800);
+    });
+
+    $('.button.another').click(getTask);
+
+    function getTask() {
+        $.getJSON( "/radness", function( data ) { 
+            console.log(data);
+            $('#mission').html(data.task+' <em>'+data.deadline+'</em>');
+            window.location.hash = data.id;
+        });
+    };
 });
